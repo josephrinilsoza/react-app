30327356c741d762aaf6a9c0dba41730135d12b5
josephrinilsoza <josephrinil007@gmail.com>
2020-08-02 15:57:53 +0530

    Updates

---
+++ precache-manifest.d301b14072f9a8e5a5f3b90cdc905ecb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add636ec444d5a3c726232ed485156c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "befa6696db243584257a",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "46714c4e291a18ebf3ce",
+    "url": "/static/css/main.3e06297d.chunk.css"
+  },
+  {
+    "revision": "befa6696db243584257a",
+    "url": "/static/js/2.773863b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.773863b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46714c4e291a18ebf3ce",
+    "url": "/static/js/main.62186e3c.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
